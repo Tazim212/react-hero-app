b0e81b0776aa5e178d397db26b864a3635293de3
Tazim212 <iftekhartazim17@gmail.com>
2026-03-25 23:55:03 +0600

    recharts part done

--- src/components/AppDetails/AppDetails.jsx
+++ src/components/AppDetails/AppDetails.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import appData from '../../../public/apps.json'
 import { useParams } from 'react-router';
 import { FaDownload, FaStar } from 'react-icons/fa';
 import { AiFillLike } from 'react-icons/ai';
+import { ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 
 const AppDetails = () => {
@@ -11,50 +11,70 @@ const AppDetails = () => {
 
     const singleAppDetails = appData.find(item => item.id === parseInt(id))
 
-    // console.log(finding);
+    const chartData = singleAppDetails.ratings.map(item =>({name: item.name, reviews: singleAppDetails.reviews}))
+
     return (
-        <div className="bg-gray-200 min-h-[300px] text-black">
-            <div className="flex items-center gap-9">
-                <img
-                    src={singleAppDetails.image}
-                    className="rounded-lg shadow-2xl ml-16 pt-12 mb-7"
-                />
-                <div>
-                    <h1 className="text-5xl font-bold pb-10">{singleAppDetails.title}</h1>
+        <div>
+            <div className="bg-gray-200 min-h-75 text-black">
+                <div className="flex items-center gap-9">
+                    <img
+                        src={singleAppDetails.image}
+                        className="rounded-lg shadow-2xl ml-16 pt-12 mb-7"
+                    />
+                    <div>
+                        <h1 className="text-5xl font-bold pb-10">{singleAppDetails.title}</h1>
 
-                    <div className='flex justify-start items-center gap-10'>
+                        <div className='flex justify-start items-center gap-10'>
 
-                        <div className='flex-col justify-center items-center'>
-                            <span className='font-semibold'><FaDownload className='text-2xl text-green-500'></FaDownload> Downloads</span>
-                            <p className='text-3xl font-bold'>
-                                {singleAppDetails.downloads}
-                            </p>
+                            <div className='flex-col justify-center items-center'>
+                                <span className='font-semibold'><FaDownload className='text-2xl text-green-500'></FaDownload> Downloads</span>
+                                <p className='text-3xl font-bold'>
+                                    {singleAppDetails.downloads}
+                                </p>
+                            </div>
+                            <div className='flex-col justify-center items-center'>
+                                <span className='font-semibold'><FaStar className='text-2xl text-yellow-500'></FaStar> Ratings</span>
+                                <p className='text-3xl font-bold'>
+                                    {singleAppDetails.ratingAvg}
+                                </p>
+                            </div>
+                            <div className='flex-col justify-center items-center'>
+                                <span className='font-semibold'><AiFillLike className='text-2xl text-blue-500'></AiFillLike> Reviews</span>
+                                <p className='text-3xl font-bold'>
+                                    {singleAppDetails.reviews}
+                                </p>
+                            </div>
                         </div>
-                        <div className='flex-col justify-center items-center'>
-                            <span className='font-semibold'><FaStar className='text-2xl text-yellow-500'></FaStar> Ratings</span>
-                            <p className='text-3xl font-bold'>
-                                {singleAppDetails.ratingAvg}
-                            </p>
-                        </div>
-                        <div className='flex-col justify-center items-center'>
-                            <span className='font-semibold'><AiFillLike className='text-2xl text-blue-500'></AiFillLike> Reviews</span>
-                            <p className='text-3xl font-bold'>
-                                {singleAppDetails.reviews}
-                            </p>
-                        </div>
+
+                        <button className="btn btn-success px-5 mt-6">Install Now (250 MB)</button>
                     </div>
-
-                    <button className="btn btn-success px-5 mt-6">Install Now (250 MB)</button>
                 </div>
             </div>
+
+            <ComposedChart
+                style={{  aspectRatio: 1.618, backgroundColor: "wheat", margin: "16px 20px"}}
+                height={400}
+                width={600}
+                responsive
+                data={chartData}
+                margin={{
+                    top: 20,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                }}
+            >
+                <CartesianGrid stroke="#f5f5f5" />
+                <XAxis dataKey="name" scale="band" />
+                <YAxis/>
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="reviews" barSize={40} fill="#413ea0" />
+                <Line type="monotone" dataKey="reviews" stroke="#ff7300" />
+            </ComposedChart>
+
         </div>
     );
 };
 
 export default AppDetails;
-
-// Show app image on the left.
-// Display app details such as title, rating, downloads, reviews.
-// Include an Install button:
-// When clicked, it becomes disabled and the text changes to Installed.
-// Show a Success Toast after App installed
