186b378c6fdc41b69ae21219e7f51f0e245adf98
Tazim212 <iftekhartazim17@gmail.com>
2026-03-26 22:48:19 +0600

    sorting part done

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -6,16 +6,37 @@ const AppContainer = () => {
 
     const [apps, setApps] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("apps.json")
-        .then(res =>res.json())
-        .then(data => setApps(data))
+            .then(res => res.json())
+            .then(data => setApps(data))
     }, [])
+ 
+    const handleSorting = e => {
+        const sortInput = e.target.value;
+        const sortValue = [...apps]
+
+        if (sortInput === "highToLow") {
+            sortValue.sort((a, b) => b.downloads - a.downloads)
+        }
+        else if (sortInput === "lowToHigh") {
+            sortValue.sort((a, b) => a.downloads - b.downloads)
+        }
+        setApps(sortValue)
+    }
 
     return (
         <div>
             <h1 className='my-4 text-2xl text-center font-bold'>Trending Apps</h1>
             <p className='text-center mb-3'>Explore All Trending Apps on the Market developed by us</p>
+
+            <div className='text-center my-4'>
+                <select defaultValue="Sort By" onChange={handleSorting} className="select bg-gray-400 text-white">
+                    <option disabled={true}>Sort By</option>
+                    <option value="highToLow">High To Low</option>
+                    <option value="lowToHigh">Low To High</option>
+                </select>
+            </div>
 
             <div className='grid grid-cols-1 md:grid-cols-4'>
                 {
